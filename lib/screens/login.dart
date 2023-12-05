abe680a89f2b3493e3eec0b9d28cf43264f16e9e
riozulfandy <rio.zulfandy07@gmail.com>
2023-12-05 23:59:17 +0700

    Finish design login and register page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -37,113 +37,144 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final request = context.watch<CookieRequest>();
     return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text(
-            'Login',
-          ),
-        ),
-        backgroundColor: Colors.brown,
-        foregroundColor: Colors.white,
-      ),
+      appBar: AppBar(),
       body: Container(
-        padding: const EdgeInsets.all(16.0),
+        padding: EdgeInsets.only(left: 25, right: 25, top: 23, bottom: 23),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            SizedBox(
+                child: Text(
+              'Login',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
+                height: 0.03,
+              ),
+            )),
+            const SizedBox(height: 30.0),
             TextField(
               controller: _usernameController,
               decoration: const InputDecoration(
                 labelText: 'Username',
               ),
             ),
-            const SizedBox(height: 12.0),
+            const SizedBox(height: 30.0),
             TextField(
               controller: _passwordController,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 labelText: 'Password',
-              ),
-              obscureText: true,
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(
-                      Color.fromARGB(255, 31, 141, 29)),
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    // Choose the icon based on password visibility
+                    _obscureText ? Icons.visibility : Icons.visibility_off,
+                  ),
+                  onPressed: _togglePasswordVisibility,
                 ),
-                onPressed: () async {
-                  String username = _usernameController.text;
-                  String password = _passwordController.text;
-
-                  // Cek kredensial
-                  // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
-                  // Untuk menyambungkan Android emulator dengan Django pada localhost,
-                  // gunakan URL http://10.0.2.2/
-                  final response = await request.login(
-                      "https://readandbrew-c08-tk.pbp.cs.ui.ac.id/auth/login/",
-                      {
-                        'username': username,
-                        'password': password,
-                      });
-
-                  if (request.loggedIn) {
-                    user_id = response['id'];
-                    user_username = response['username'];
-                    user_status = response['user_status'];
-                    String message = response['message'];
-                    String uname = response['username'];
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(builder: (context) => MyHomePage()),
-                    );
-                    ScaffoldMessenger.of(context)
-                      ..hideCurrentSnackBar()
-                      ..showSnackBar(SnackBar(
-                          content: Text("$message Selamat datang, $uname.")));
-                  } else {
-                    showDialog(
-                      context: context,
-                      builder: (context) => AlertDialog(
-                        title: const Text('Login Gagal'),
-                        content: Text(response['message']),
-                        actions: [
-                          TextButton(
-                            child: const Text('OK'),
-                            onPressed: () {
-                              Navigator.pop(context);
-                            },
-                          ),
-                        ],
+              ),
+              obscureText: _obscureText,
+            ),
+            const SizedBox(height: 61.0),
+            Container(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF377C35)),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
+                    ),
+                  ),
+                  onPressed: () async {
+                    String username = _usernameController.text;
+                    String password = _passwordController.text;
+
+                    // Cek kredensial
+                    // TODO: Ganti URL dan jangan lupa tambahkan trailing slash (/) di akhir URL!
+                    // Untuk menyambungkan Android emulator dengan Django pada localhost,
+                    // gunakan URL http://10.0.2.2/
+                    final response = await request.login(
+                        "https://readandbrew-c08-tk.pbp.cs.ui.ac.id/auth/login/",
+                        {
+                          'username': username,
+                          'password': password,
+                        });
+
+                    if (request.loggedIn) {
+                      user_id = response['id'];
+                      user_username = response['username'];
+                      user_status = response['user_status'];
+                      String message = response['message'];
+                      String uname = response['username'];
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(builder: (context) => MyHomePage()),
+                      );
+                      ScaffoldMessenger.of(context)
+                        ..hideCurrentSnackBar()
+                        ..showSnackBar(SnackBar(
+                            content: Text("$message Selamat datang, $uname.")));
+                    } else {
+                      showDialog(
+                        context: context,
+                        builder: (context) => AlertDialog(
+                          title: const Text('Login Gagal'),
+                          content: Text(response['message']),
+                          actions: [
+                            TextButton(
+                              child: const Text('OK'),
+                              onPressed: () {
+                                Navigator.pop(context);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  )),
+            ),
+            const SizedBox(height: 10.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Don't have an account?"),
+                TextButton(
+                  onPressed: () async {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const RegisterFormPage()),
                     );
-                  }
-                },
-                child: const Text(
-                  'Login',
-                  style: TextStyle(color: Colors.white),
-                )),
-            const SizedBox(height: 12.0),
-            ElevatedButton(
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(
-                    const Color.fromARGB(255, 14, 104, 179)),
-              ),
-              onPressed: () async {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => const RegisterFormPage()),
-                );
-              },
-              child: const Text(
-                'Register',
-                style: TextStyle(color: Colors.white),
-              ),
+                  },
+                  child: Text(
+                    'Register',
+                    style: TextStyle(
+                      color: Color(0xFF377C35),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ],
         ),
