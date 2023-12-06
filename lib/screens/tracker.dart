9420c1df490ad8b6c6c380fa126a8eab57df2d89
bulan <bulan.athaillah@gmail.com>
2023-12-06 16:29:12 +0700

    fix integration, create card to display book

--- lib/screens/tracker.dart
+++ lib/screens/tracker.dart
@@ -1,10 +1,10 @@
-//TODO: figure out import book detail dari Django
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
 import 'dart:convert';
 import 'package:read_and_brew/models/booktracker.dart';
+import 'package:read_and_brew/models/buku.dart';
 import 'package:read_and_brew/widgets/left_drawer.dart';
 
 String getStatusLabel(String status) {
@@ -46,147 +46,241 @@ class _BookTrackerPageState extends State<BookTrackerPage> {
     }
     return list_book_tracker;
   }
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(
-      title: const Text('Book Tracker'),
-      backgroundColor: Colors.indigo,
-      foregroundColor: Colors.white,
-    ),
-    drawer: const LeftDrawer(),
-    body: FutureBuilder(
-      future: fetchBook(),
-      builder: (context, AsyncSnapshot snapshot) {
-        if (snapshot.data == null) {
-          return const Center(child: CircularProgressIndicator());
-        } else {
-          if (!snapshot.hasData) {
-            return const Column(
-              children: [
-                Text(
-                  "You haven't tracked any books.",
-                  style: TextStyle(color: Color(0xff59A5D8), fontSize: 20),
-                ),
-                SizedBox(height: 8),
-              ],
-            );
-          } else {
-            return ListView.builder(
-              itemCount: snapshot.data!.length,
-              itemBuilder: (_, index) => InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          DetailPage(bookTracker: snapshot.data![index]),
-                    ),
-                  );
-                },
-                child: Container(
-                  margin:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
-                  padding: const EdgeInsets.all(20.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        getStatusLabel(snapshot.data![index].fields.status),
-                        style: const TextStyle(
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      // const SizedBox(height: 10),
-                      // Text("${snapshot.data![index].fields.penulis}"),
-                      // const SizedBox(height: 10),
-                      // Text("${snapshot.data![index].fields.kategori}")
-                    ],
-                  ),
-                ),
-              ),
-            );
-          }
-        }
-      },
-    ),
-  );
+
+  Future<Buku> fetchBookDetails(int bookId) async {
+    var url = Uri.parse(
+        'https://readandbrew-c08-tk.pbp.cs.ui.ac.id/booklist/api/buku/');
+    var response = await http.get(
+      url,
+      headers: {"Content-Type": "application/json"},
+    );
+
+    var data = jsonDecode(utf8.decode(response.bodyBytes));
+
+    // find the book with the desired ID
+    var bookData =
+        data.firstWhere((book) => book['pk'] == bookId, orElse: () => null);
+
+    if (bookData != null) {
+      return Buku.fromJson(bookData);
+    } else {
+      throw Exception('Book not found');
+    }
   }
-}
-
-class DetailPage extends StatelessWidget {
-  final BookTracker bookTracker;
-
-  const DetailPage({Key? key, required this.bookTracker}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Test"),
+        title: const Text('Book Tracker'),
+        backgroundColor: Colors.brown,
+        foregroundColor: Colors.white,
+      ),
+      drawer: const LeftDrawer(),
+      body: FutureBuilder(
+        future: fetchBook(),
+        builder: (context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return const Center(child: CircularProgressIndicator());
+          } else {
+            if (!snapshot.hasData) {
+              return Column(
+                children: [
+                  Text(
+                    "You haven't tracked any books.",
+                    style: TextStyle(color: Color.fromARGB(255, 133, 67, 13)),
+                  ),
+                  SizedBox(height: 8),
+                ],
+              );
+            } else {
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 16.0,
+                  mainAxisSpacing: 16.0,
+                  childAspectRatio: (1 / 1.5),
+                ),
+                itemCount: snapshot.data!.length,
+                itemBuilder: (_, index) => InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DetailPage(
+                            bookTracker: snapshot.data![index],
+                            fetchBookDetails: (bookId) =>
+                                fetchBookDetails(bookId)),
+                      ),
+                    );
+                  },
+                  child: Card(
+                    elevation: 4.0,
+                    child: IntrinsicHeight(
+                      child: Container(
+                        padding: const EdgeInsets.all(16.0),
+                        child: FutureBuilder(
+                          future: fetchBookDetails(
+                              snapshot.data![index].fields.book),
+                          builder: (context, AsyncSnapshot<Buku> bookSnapshot) {
+                            if (bookSnapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return const CircularProgressIndicator();
+                            } else if (bookSnapshot.hasError) {
+                              return Text('Error loading book details');
+                            } else if (!bookSnapshot.hasData) {
+                              return Text('No book details available');
+                            } else {
+                              // Access penulis and kategori from bookSnapshot.data.fields
+                              return Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Expanded(
+                                    child: Image.network(
+                                      bookSnapshot.data!.fields.gambar,
+                                      fit: BoxFit
+                                          .cover, // This will cover the card's width
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          bookSnapshot.data!.fields.judul,
+                                          style: const TextStyle(
+                                            fontSize: 18.0,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          bookSnapshot.data!.fields.penulis,
+                                          style:
+                                              const TextStyle(fontSize: 14.0),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                        Text(
+                                          getStatusLabel(snapshot
+                                              .data![index].fields.status),
+                                          style:
+                                              const TextStyle(fontSize: 14.0),
+                                          overflow: TextOverflow.ellipsis,
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ],
+                              );
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            }
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  final BookTracker bookTracker;
+  final Function(int) fetchBookDetails;
+
+  const DetailPage({
+    Key? key,
+    required this.bookTracker,
+    required this.fetchBookDetails,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Book Tracker"),
+        
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Image.network(
-            //   bookTracker.fields.gambar, // Replace this with your image URL
-            //   width: 100.0, // Adjust the width as needed
-            //   height: 200.0, // Adjust the height as needed
-            //   fit: BoxFit.cover, // You can adjust the fit property based on your needs
-            // ),
-            // Text(
-            //   "${bookTracker.fields.judul}",
-            //   style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            // ),
-            // SizedBox(height: 10),
-            // Text(
-            //   "Penulis:",
-            //   style: TextStyle(fontWeight: FontWeight.bold),
-            // ),
-            // Text("${bookTracker.fields.penulis}"),
-            // SizedBox(height: 10),
-            // Text(
-            //   "Kategori:",
-            //   style: TextStyle(fontWeight: FontWeight.bold),
-            // ),
-            // Text("${bookTracker.fields.kategori}"),
-            // SizedBox(height: 10),
-            Text(
-              "Status:",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text(
-              getStatusLabel(bookTracker.fields.status),
-              style: const TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate back to the previous screen (R)
-                Navigator.pop(context);
-              },
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0), // Set button shape to rectangle
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 16), // Adjust padding for a smaller button
-                child: Text(
-                  'Back',
-                  style: TextStyle(fontSize: 14, color: Colors.white), // Set text color to white
-                ),
-              ),
-            ),
-          ],
+        child: FutureBuilder(
+          future: fetchBookDetails(bookTracker.fields.book),
+          builder: (context, AsyncSnapshot<Buku> bookSnapshot) {
+            if (bookSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(child: CircularProgressIndicator());
+            } else if (bookSnapshot.hasError) {
+              return Center(child: Text('Error loading book details'));
+            } else if (!bookSnapshot.hasData) {
+              return Center(child: Text('No book details available'));
+            } else {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Image.network(
+                    bookSnapshot.data!.fields.gambar,
+                    width: 100.0,
+                    height: 200.0,
+                    fit: BoxFit.cover,
+                  ),
+                  Text(
+                    "${bookSnapshot.data!.fields.judul}",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(height: 10),
+                  Text(
+                    "Penulis:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("${bookSnapshot.data!.fields.penulis}"),
+                  SizedBox(height: 10),
+                  Text(
+                    "Kategori:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text("${bookSnapshot.data!.fields.kategori}"),
+                  SizedBox(height: 10),
+                  Text(
+                    "Status:",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    getStatusLabel(bookTracker.fields.status),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Navigate back to the previous screen
+                      Navigator.pop(context);
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.brown, 
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      child: Text(
+                        'Back',
+                        style: TextStyle(fontSize: 14, color: Color(0xFFFFFFFF)),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            }
+          },
         ),
       ),
     );
