56ed31755353aab955f97d02d2a3f522e052624f
Alexander Audric Johansyah <audricjohansyah@gmail.com>
2023-12-16 19:11:38 +0700

    Updating designs for employee inventory page, adding pictures

--- lib/screens/ordernborrow screens/employee/inventory.dart
+++ lib/screens/ordernborrow screens/employee/inventory.dart
@@ -16,7 +16,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Beans",
         "description": "Freshly roasted coffee beans for the perfect brew.",
-        "amount": 50
+        "amount": 50,
+        "picture":
+            "https://media.istockphoto.com/id/1249763975/photo/fresh-roasted-coffee-beans-with-burlap-sack-on-a-wooden-table.jpg?s=612x612&w=0&k=20&c=BC_P1VR0_IJ9DRkb2dbZ1sKN2U4eZGn73kxdh9uJFD4="
       }
     },
     {
@@ -24,7 +26,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Espresso Machine",
         "description": "High-quality espresso machine for your coffee needs.",
-        "amount": 5
+        "amount": 5,
+        "picture":
+            "https://media.istockphoto.com/id/1298501845/photo/close-up-barista-hand-inserts-the-metal-filter-in-the-coffee-machine-waiting-for-pouring-hot.jpg?s=612x612&w=0&k=20&c=Yey3fVCCAFlXuac9x8J7yh-emzYR0DiGt74wcpNNlyY="
       }
     },
     {
@@ -33,7 +37,9 @@ class _InventoryPageState extends State<InventoryPage> {
         "name": "Milk Frother",
         "description":
             "Efficient milk frother to create creamy lattes and cappuccinos.",
-        "amount": 10
+        "amount": 10,
+        "picture":
+            "https://media.istockphoto.com/id/1291792084/photo/mixing-milk-in-glass-by-milk-frother-making-foamy-milk-with-frother-milk-handheld-mixer.jpg?s=612x612&w=0&k=20&c=up0VOGsXGuR3kJ2Pz4s5QbxcdYCNFBt7HgfDjZbHJCA="
       }
     },
     {
@@ -41,7 +47,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Cups",
         "description": "Disposable coffee cups for takeout orders.",
-        "amount": 200
+        "amount": 200,
+        "picture":
+            "https://media.istockphoto.com/id/472113574/photo/cups-of-coffee.jpg?s=612x612&w=0&k=20&c=J8-_LZt6Nzne6Afg0amnBnBuWG5kyj2Emnd6NY9GsRk="
       }
     },
     {
@@ -49,7 +57,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Filters",
         "description": "Coffee filters for your drip coffee maker.",
-        "amount": 1000
+        "amount": 1000,
+        "picture":
+            "https://images.squarespace-cdn.com/content/v1/5957d4148419c29314283239/1499458134301-XXM4VD3X64OSIKVTXE7P/Kalita.jpg"
       }
     },
     {
@@ -57,7 +67,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Tea Bags",
         "description": "Assorted tea bags for a variety of flavors.",
-        "amount": 100
+        "amount": 100,
+        "picture":
+            "https://media.istockphoto.com/id/499262293/photo/jasmine-green-tea.jpg?s=612x612&w=0&k=20&c=T5PctoAEL1PUrKBRBetmH6lO2ZnkPWqQ_MY591YaYKI="
       }
     },
     {
@@ -65,7 +77,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Grinder",
         "description": "Professional coffee grinder for fresh coffee grounds.",
-        "amount": 8
+        "amount": 8,
+        "picture":
+            "https://media.istockphoto.com/id/1319489361/photo/close-up-of-portafilter-in-hands-of-barista-into-which-coffee-grinder-pours-ground-coffee.jpg?s=612x612&w=0&k=20&c=ciSyhcW4D8ISsJPv3kvftLloiqByCzeZ2MijBHRZ_cI="
       }
     },
     {
@@ -73,7 +87,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Syrup Dispenser",
         "description": "Dispenser for flavored syrups for your beverages.",
-        "amount": 15
+        "amount": 15,
+        "picture":
+            "https://media.istockphoto.com/id/1134667542/photo/row-of-pump-head-bottle-of-beverage-or-syrup-in-cafe.jpg?s=612x612&w=0&k=20&c=kG77BMY9mN5rDe_1xRnq96exsSl3wmz9OCJrRumMAVs="
       }
     },
     {
@@ -81,7 +97,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Stirrers",
         "description": "Wooden coffee stirrers for stirring hot beverages.",
-        "amount": 500
+        "amount": 500,
+        "picture":
+            "https://media.istockphoto.com/id/1128129200/photo/stir-sticks-and-coffee-beans.jpg?s=612x612&w=0&k=20&c=W5WmbpUgGYoLTqm2bubezDYCg5V-eoDHrZwkJ6-tw70="
       }
     },
     {
@@ -89,7 +107,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Sugar Packets",
         "description": "Individual sugar packets for sweetening drinks.",
-        "amount": 300
+        "amount": 300,
+        "picture":
+            "https://media.istockphoto.com/id/1125582977/photo/pouring-sugar-from-packet-into-coffee-cup.jpg?s=612x612&w=0&k=20&c=gCBCLR2LwX-XWjkpFHPoYrxsd8vsWwh6C868xtXDLAg="
       }
     },
     {
@@ -97,7 +117,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Hot Water Kettle",
         "description": "Electric kettle for boiling water quickly.",
-        "amount": 6
+        "amount": 6,
+        "picture":
+            "https://www.shutterstock.com/image-photo/transparent-electric-kettle-boiling-water-600nw-1556484236.jpg"
       }
     },
     {
@@ -105,7 +127,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Espresso Cups",
         "description": "Espresso shot cups for serving espresso shots.",
-        "amount": 50
+        "amount": 50,
+        "picture":
+            "https://i2.wp.com/blog.lacolombe.com/wp-content/uploads/2015/10/espresso-copy.jpg"
       }
     },
     {
@@ -113,7 +137,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Lids for Cups",
         "description": "Plastic lids for coffee cups to prevent spills.",
-        "amount": 400
+        "amount": 400,
+        "picture":
+            "https://st2.depositphotos.com/25706078/46416/i/450/depositphotos_464163824-stock-photo-two-paper-cups-lid-park.jpg"
       }
     },
     {
@@ -121,7 +147,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Napkins",
         "description": "Paper napkins for customers to use.",
-        "amount": 1000
+        "amount": 1000,
+        "picture":
+            "https://media.istockphoto.com/id/186843509/photo/cocktail-napkin-on-wood.jpg?s=612x612&w=0&k=20&c=tNiBOIaqG8Oz5yJbamnMjHEATysE_BSuccJAcB3mrHs="
       }
     },
     {
@@ -129,7 +157,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Spoons",
         "description": "Small coffee spoons for stirring hot drinks.",
-        "amount": 600
+        "amount": 600,
+        "picture":
+            "https://media.istockphoto.com/id/618972606/photo/coffee-beans-roasted-instant-and-grinded-coffee.jpg?s=612x612&w=0&k=20&c=yUMNjrUfTngv5sLhuT3O8-VEpnBgZm7UPzJJinXZsAg="
       }
     },
     {
@@ -137,7 +167,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Coffee Creamer",
         "description": "Dairy and non-dairy creamer options for coffee.",
-        "amount": 40
+        "amount": 40,
+        "picture":
+            "https://media.istockphoto.com/id/1216174435/photo/barista-hand-making-cappuccino-coffee-with-espresso-machine-in-cafe.jpg?s=612x612&w=0&k=20&c=LZtSttGjwkvrQCl-YpCt_7CUx4Z03KBMTj16jvo1sKM="
       }
     },
     {
@@ -145,7 +177,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Cocoa Powder",
         "description": "High-quality cocoa powder for making hot chocolate.",
-        "amount": 20
+        "amount": 20,
+        "picture":
+            "https://media.istockphoto.com/id/1296362170/photo/detail-of-cocoa-fruit-with-pieces-of-chocolate-and-cocoa-powder-on-raw-cocoa-beans.jpg?s=612x612&w=0&k=20&c=_t5-x-t-hHJiBZTXcM9Nb_CjUF7wAFVLX0MXDVrsDaA="
       }
     },
     {
@@ -153,7 +187,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Disposable Stirrers",
         "description": "Disposable stirrers for hot and cold drinks.",
-        "amount": 500
+        "amount": 500,
+        "picture":
+            "https://media.istockphoto.com/id/897221182/photo/brown-plastic-stir-sticks.jpg?s=612x612&w=0&k=20&c=UYIM9v4dkOAzS3RYWTSo1xU-qtTAESb9mu15ZqFgUEM="
       }
     },
     {
@@ -161,7 +197,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Tea Infusers",
         "description": "Tea infusers for loose leaf tea brewing.",
-        "amount": 30
+        "amount": 30,
+        "picture":
+            "https://media.istockphoto.com/id/467293044/photo/tea-ball-with-black-tea-and-red-fruit.jpg?s=612x612&w=0&k=20&c=72Gp_cXzOew7mhyFOliDTHH2abwLlM1rUQg5ZwouBXE="
       }
     },
     {
@@ -169,16 +207,19 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Lemon Slices",
         "description": "Slices of fresh lemon for garnishing drinks.",
-        "amount": 100
+        "amount": 100,
+        "picture":
+            "https://media.istockphoto.com/id/175592069/photo/lemon-background.jpg?s=170667a&w=0&k=20&c=GAnJNzIsinuV92_lXqwNVpqVYZFg24Tzzxz36A6fuaE="
       }
     },
     {
       "pk": 21,
       "fields": {
-        "name": "Honey Packets",
-        "description":
-            "Individual honey packets for sweetening tea and coffee.",
-        "amount": 150
+        "name": "Honey Pots",
+        "description": "Honey pots for sweetening tea and coffee.",
+        "amount": 150,
+        "picture":
+            "https://media.istockphoto.com/id/598241944/photo/honey-in-jar-and-bunch-of-dry-lavender.jpg?s=612x612&w=0&k=20&c=gVg1BaJ78uniQbpfdFiYvMzim98gREdx-5c4ENBp2tE="
       }
     },
     {
@@ -186,7 +227,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Ice Scoops",
         "description": "Ice scoops for adding ice to cold beverages.",
-        "amount": 25
+        "amount": 25,
+        "picture":
+            "https://media.istockphoto.com/id/138143844/photo/a-photograph-of-a-bucket-of-ice-with-a-trowel.jpg?s=612x612&w=0&k=20&c=qoKM-hHihgSsglkGq2uo5SyjiZy4Ygzq59kFZpkaP-E="
       }
     },
     {
@@ -194,7 +237,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Straws",
         "description": "Plastic straws for cold drinks.",
-        "amount": 1000
+        "amount": 1000,
+        "picture":
+            "https://media.istockphoto.com/id/1175305045/video/hand-stirring-tasty-ice-coffee.jpg?s=640x640&k=20&c=H2EQmBt6TNCkqY8nJQ_4HWYeiaRa0b_UE3a_sSG-epU="
       }
     },
     {
@@ -202,7 +247,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Whipped Cream Dispenser",
         "description": "Dispenser for whipped cream toppings.",
-        "amount": 5
+        "amount": 5,
+        "picture":
+            "https://media.istockphoto.com/id/933035364/photo/ice-cream-machine.jpg?s=612x612&w=0&k=20&c=v22SYCiQNCeLT9lqF1ZPhQXDyp0ffPoSGRsoYPnLwj8="
       }
     },
     {
@@ -210,7 +257,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Sugar Cubes",
         "description": "Individual sugar cubes for coffee and tea.",
-        "amount": 200
+        "amount": 200,
+        "picture":
+            "https://media.istockphoto.com/id/1144176000/photo/sugar-sand-and-sugar-cubes-poured-from-a-wooden-spoon.jpg?s=612x612&w=0&k=20&c=Cf7zubWcfiJfM6r4biomdgioHn6OFdbQgHjzHqU6Q1M="
       }
     },
     {
@@ -218,7 +267,9 @@ class _InventoryPageState extends State<InventoryPage> {
       "fields": {
         "name": "Drink Napkins",
         "description": "Small drink napkins for beverage service.",
-        "amount": 800
+        "amount": 800,
+        "picture":
+            "https://media.istockphoto.com/id/814918828/photo/white-paper-napkin-on-table.jpg?s=612x612&w=0&k=20&c=LbLYFKeuazAI6OLg41z1_mvybf47gpSF1YklQ1CTB6A="
       }
     },
     {
@@ -227,7 +278,9 @@ class _InventoryPageState extends State<InventoryPage> {
         "name": "Reusable Coffee Filters",
         "description":
             "Eco-friendly reusable coffee filters for sustainable brewing.",
-        "amount": 30
+        "amount": 30,
+        "picture":
+            "https://media.istockphoto.com/id/1135173539/photo/reusable-metal-mesh-coffee-filter-for-drip-coffee-maker-permanent-basket-coffee-filter-on.jpg?s=612x612&w=0&k=20&c=dMdSbmdznVtalWGN4XfcbHp3E4QN7qGVNQSyGPM4zlE="
       }
     },
     {
@@ -236,7 +289,9 @@ class _InventoryPageState extends State<InventoryPage> {
         "name": "Matcha Powder",
         "description":
             "High-quality matcha powder for making matcha lattes and other green tea beverages.",
-        "amount": 10
+        "amount": 10,
+        "picture":
+            "https://media.istockphoto.com/id/865227762/photo/matcha-green-tea.jpg?s=612x612&w=0&k=20&c=AmdEj8LixXw_lPZu2xqn0BrtYhVZkczAckfk2HvDN5s="
       }
     }
   ];
@@ -245,9 +300,12 @@ class _InventoryPageState extends State<InventoryPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Order & Borrow - Inventory'),
-        backgroundColor: Color(0xFF377C35),
-        foregroundColor: Colors.white,
+        title: const Text(
+          'Inventory',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        foregroundColor: const Color(0xFF377C35),
+        backgroundColor: Colors.white,
       ),
       drawer: const LeftDrawer(),
       body: SizedBox(
@@ -262,6 +320,18 @@ class _InventoryPageState extends State<InventoryPage> {
                 titleGap: 20,
                 bgColor: Colors.white,
                 screenWidth: 600,
+                action: Center(
+                  child: FractionallySizedBox(
+                    widthFactor: 0.8,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.network(
+                        suppliesList['picture'],
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                ),
                 title: Text(
                   suppliesList['name'],
                   style: const TextStyle(
